53662e76146178c139b79119e8fc00c704dd5005
Joel Jaffe <joel@jaffesd.com>
2025-01-20 10:46:21 -0800

    Configured Build Env

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cpp-Playground 
+Plug-n-Play Build Env for C++ Projects. 
+
+Use `./run.sh` (or `SHIFT+CMD+B` in VSCode) to build.
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if build directory exists, if not create it
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+# Change to build directory
+cd build
+
+# Run cmake to configure the project
+cmake ..
+
+# Build the project
+cmake --build . 
